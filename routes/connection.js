f32d115d98b333ffc0ce36f90e25e0ed562ddb00
pooja <poojadamewar321@gmail.com>
2026-02-14 12:52:25 +0530

    ffhgj

--- routes/connection.js
+++ routes/connection.js
@@ -3,24 +3,13 @@ var util = require("util");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "bx123.clever-cloud.com",
+  host: "bx123456-xxx.services.clever-cloud.com", // correct host
   user: "u123456",
   password: "xxxxxxxx",
   database: "b123456",
   port: 3306,
-
   ssl: {
     rejectUnauthorized: false
-  }
-});
-
-// test connection
-pool.getConnection(function(err, connection) {
-  if (err) {
-    console.log("Database connection failed:", err);
-  } else {
-    console.log("Database connected successfully");
-    connection.release();
   }
 });
 
