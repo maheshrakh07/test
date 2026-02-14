e8cb3080d6fb5373bc409fb5aeb508e2de5e3c59
pooja <poojadamewar321@gmail.com>
2026-02-14 12:15:54 +0530

    cvb

--- routes/connection.js
+++ routes/connection.js
@@ -3,10 +3,11 @@ var util = require("util");
 
 
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"dent_care"
+  host: "your-host",
+  user: "your-user",
+  password: "your-password",
+  database: "your-database",
+  port: 3306
 });
 
 var exe = util.promisify(conn.query).bind(conn)
