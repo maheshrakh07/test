9d91e4a1929c6e68472f80c2438d9cd5bbb83869
pooja <poojadamewar321@gmail.com>
2026-02-14 13:09:20 +0530

    asdfg

--- routes/connection.js
+++ routes/connection.js
@@ -1,18 +1,25 @@
-var mysql = require("mysql");
-var util = require("util");
+// var mysql = require("mysql");
+// var util = require("util");
 
-var pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "btipauflfbkrtdk3sisp-mysql.services.clever-cloud.com", // correct host
-  user: "u8tszmrmmmgfti8k",
-  password: "xxxxxxxx",
-  database: "u8tszmrmmmgfti8k",
-  port: 3306,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// var pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "btipauflfbkrtdk3sisp-mysql.services.clever-cloud.com", // correct host
+//   user: "u8tszmrmmmgfti8k",
+//   password: "xxxxxxxx",
+//   database: "u8tszmrmmmgfti8k",
+//   port: 3306,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-var exe = util.promisify(pool.query).bind(pool);
+// var exe = util.promisify(pool.query).bind(pool);
 
-module.exports = exe;
+// module.exports = exe;
+
+
+async function exe(query) {
+  return [];
+}
+
+module.exports = exe; 
